c5c8e3b1e4db276c894d5dab7c7c080a442c8cf9
godfreymaina <itsgoddy4pf@gmail.com>
2026-06-24 17:41:55 +0300

    fixed the footer

--- src/components/Home/footer.jsx
+++ src/components/Home/footer.jsx
@@ -1,29 +1,51 @@
+
+import { IoCall } from "react-icons/io5";
+import { IoIosMail } from "react-icons/io";
+import { FaGithub, FaLinkedin, FaDiscord } from "react-icons/fa";
+
 const Footer = () => {
     return (
-        <>
-            <div className="container-fluid bg-dark">
-                <div className="container">
-                    <div className="row text-light">
-                        <div className="col-md-4 mt-3">
-                            <h5 className="border-bottom py-3">About us</h5>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit ea omnis alias, beatae temporibus non enim animi, incidunt, laborum ab dolorum? Fugit magni minus quisquam itaque eaque molestiae obcaecati? Vitae!</p>
+          <div className="container-fluid bg-dark">
+            <div className="container">
+                <div className="row text-light">
+                    <div className="col-md-4 mt-3">
+                        <h5 className="border-bottom py-3">
+                            About us
+                        </h5>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit cupiditate incidunt fuga nemo dolore quidem hic?
+                        </p>
+                    </div>
+                    <div className="col-md-4 mt-3">
+                        <h5 className="border-bottom py-3">
+                            Reach Us Through
+                        </h5>
+                        <h6 className="fw-bold">
+                          <span className="me-3"> <IoCall /></span>+2547939373673
+                        </h6>
+                        <h6 className="fw-bold">
+                          <span className="me-3"> <IoIosMail /></span>info@blogapp.com
+                        </h6>
+                    </div>
+                    <div className="col-md-4 mt-3">
+                        <h5 className="border-bottom py-3">
+                            Our Social Handles
+                        </h5>
+                        <div className="d-flex">
+                            <h5 className="m-3">
+                                <FaGithub />
+                            </h5>
+                            <h5 className="m-3">
+                                <FaLinkedin />
+                            </h5>
+                            <h5 className="m-3">
+                                <FaDiscord />
+                            </h5>
                         </div>
-                        <div className="col-md-4">
-                            <h5 className="border-bottom py-3">Reach us through</h5>
-                            <h6 className="fw-bold">
-                                <span className="me-3">11</span>+25423242422
-                            </h6>
-                            <h6 className="fw-bold">
-                                <span className="me-3">22</span> info@blogapp.com
-                            </h6>
-                        </div>
-                        <div className="col-md-4"></div>
                     </div>
                 </div>
             </div>
-
-
-        </>
+        </div>
     );
 }
 
